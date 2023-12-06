c459877010746c3d5f02afde80a97d5e299b277e
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2023-12-06 10:45:23 +0000

    Days 4, 5, 6 complete

---
+++ day_06/part1.py
@@ -0,0 +1,30 @@
+import os, re, math
+
+def get_input(name="input"):
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(current_directory, name)
+    with open(input_file_path, 'r') as file:
+        input_file = file.read().strip().split('\n')
+    
+    return input_file
+
+def main():
+    input_file = get_input()
+    
+    times = list(map(int, re.findall(r'\d+', input_file[0])))
+    distances = list(map(int, re.findall(r'\d+', input_file[1])))
+    
+    prod = 1
+    for time, distance in zip(times, distances):
+        disc = math.sqrt(time**2 - 4*distance)
+        lower = math.ceil((time - disc) * 0.5+0.00001)
+        upper = math.floor((time + disc) * 0.5 - 0.00001)
+        prod *= upper - lower + 1
+        
+    print(prod)
+        
+
+
+
+if __name__ == "__main__":
+    main()
